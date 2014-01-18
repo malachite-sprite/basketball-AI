0aef888c72d85f5e86bda4219f8e2b94108fe353
malachite-sprite <m4rch3r@gmail.com>
2014-01-18 16:46:08 -0600

    correcting minor changes

---
+++ Pattern.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Pattern.py
+
+The basic pattern. Each pattern consists of a list of player positions (See position.py)
+
+"""
+
+import Position.py
+
+class Pattern:
+	def __init__(self):
+		pass
+	
+	def 
