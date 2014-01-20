59c7efc2ac25c41d38cfd6eaabfc4f3f451080ec
malachite-sprite <m4rch3r@gmail.com>
2014-01-19 22:48:18 -0600

    rearanged team loading to make mutual memories more efficient

--- Team.py
+++ Team.py
@@ -13,12 +13,18 @@ class Team:
     initial_positions = [ Position( ... ) ]
     
     roster = []
+    memory = []
     
     def __init__( self, filename, basket_location, number_of_players=5 ):
         if basket_location is not "+" or "-":
             print "Basket location needs to be either '+' or '-'!"
             break
         else:
+            try:    
+                for i in range( number_of_players ):
+                    roster.append( Player( team, initial_positions[ i ], basket_location ) )
+            except:
+                "Not enough initial positions specified to support this number of players!"
             try:
                 open( filename, "r" ) as temp:
                 line = ""
@@ -26,11 +32,6 @@ class Team:
                 while line is not "endfile":
                     memories.append( PAR.from_string( line ) )
                     line = temp.next()
-                try:    
-                    for i in range( number_of_players ):
-                        roster.append( Player( memories, initial_positions[ i ], basket_location ) )
-                except:
-                    "not enough initial positions programmed to support this number of players"
                 temp.close()
             except Exception as hell:
                 print "Whoops, something broke when trying to read from " + filename + "!"
