5599f4149f62e5a214fe7ba53dcbb79be6f58940
malachite-sprite <m4rch3r@gmail.com>
2014-01-19 23:04:34 -0600

    removed direction as a player specific variable; it is now team specific

--- Team.py
+++ Team.py
@@ -14,28 +14,26 @@ class Team:
     
     roster = []
     memory = []
+    basket_location = None
     
-    def __init__( self, filename, basket_location, number_of_players=5 ):
-        if basket_location is not "+" or "-":
-            print "Basket location needs to be either '+' or '-'!"
-            break
-        else:
-            try:    
-                for i in range( number_of_players ):
-                    roster.append( Player( team, initial_positions[ i ], basket_location ) )
-            except:
-                "Not enough initial positions specified to support this number of players!"
-            try:
-                open( filename, "r" ) as temp:
-                line = ""
-                memories = []
-                while line is not "endfile":
-                    memories.append( PAR.from_string( line ) )
-                    line = temp.next()
-                temp.close()
-            except Exception as hell:
-                print "Whoops, something broke when trying to read from " + filename + "!"
-                raise hell
+    def __init__( self, filename, _basket_location, number_of_players=5 ):
+        basket_location = _basket_location
+        try:    
+            for i in range( number_of_players ):
+                roster.append( Player( team, initial_positions[ i ] ) )
+        except:
+            "Not enough initial positions specified to support this number of players!"
+        try:
+            open( filename, "r" ) as temp:
+            line = ""
+            memories = []
+            while line is not "endfile":
+                memories.append( PAR.from_string( line ) )
+                line = temp.next()
+            temp.close()
+        except Exception as hell:
+            print "Whoops, something broke when trying to read from " + filename + "!"
+            raise hell
     
     def save( self ):
         pass
