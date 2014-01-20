bc9b958d8e5e9ef661a048c457014b2c7622ed34
malachite-sprite <m4rch3r@gmail.com>
2014-01-20 11:10:55 -0600

    added init functionality to game class

--- Game.py
+++ Game.py
@@ -3,9 +3,16 @@
 """
 Game.py
 """
+
+from Team import Team
+#from Display import Display
+
 class Game:
     
     number_of_players_per_team = 5
+    teams = []
     
-    def __init__( self ):
-        
+    def __init__( self, team_file_1, team_file_2, board_file ):
+        teams.append( Team( team_file_1, 1, 3 ) )
+        teams.append( Team( team_file_2, -1, 3 ) )
+    
