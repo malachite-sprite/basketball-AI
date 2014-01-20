d9dff92300836bcfc876b780b6656b54e07412b6
malachite-sprite <m4rch3r@gmail.com>
2014-01-19 22:46:42 -0600

    added possibly functionality for from string method. Will likely change when Actions are programmed

--- PAR.py
+++ PAR.py
@@ -17,3 +17,14 @@ class PAR:
         action = _action
         result = _result
     
+    def __str__( self ):
+        return = "{ %s: %s: %f }" % ( str( pattern ), str( action ), result )
+    
+    @staticmethod
+    def from_string( string ):
+        components = string.replace(" ","").replace("{","").replace("}","").split(":")
+        pattern = Pattern.from_string( components[ 0 ] )
+        action = Action.from_string( components[ 1 ] )
+        result = float( components[ 2 ] )
+        return PAR( pattern, action, result )
+    
