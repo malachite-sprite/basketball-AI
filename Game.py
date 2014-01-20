0daa1189c0aa5bbe22dc158b03872184ee308c6e
malachite-sprite <m4rch3r@gmail.com>
2014-01-19 23:06:12 -0600

    added frame for player and game classes, but little to no functionality

---
+++ Game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Game.py
+"""
+class Game:
+    
+    number_of_players_per_team = 5
+    
+    def __init__( self ):
+        
---
+++ Player.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Player.py
+"""
+class Player:
+    
+    team = None
+    position = None
+    
+    def __init__( self, _team, initial_position ):
+        team = _team
+        position = initial_position
+    
+    def act( self, state ):
+        
+    
